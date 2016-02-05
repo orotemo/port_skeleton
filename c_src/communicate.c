d6f8b97c417e4908269a9eb016688e11b85fe50b
orotemo <omri.rotem@gmail.com>
2016-02-05 18:40:27 +0200

    Update communicate.c

--- c_src/communicate.c
+++ c_src/communicate.c
@@ -6,20 +6,27 @@
 #include "communicate.h"
 #include "erl_comm.h"
 
+#define WRITE_BUF_LEN 5 * 1024 * 1000 //5MB max
+
+byte __write_buf[WRITE_BUF_LEN];
+
 void send_str_tuple(char* atom, char *str) {
-  ETERM *tup_list[2];
-  ETERM *to_send;
-  tup_list[0] = erl_mk_atom(atom);
-  tup_list[1] = erl_mk_string(str);
+  ETERM *term = erl_mk_string(str);
+  send_response_tuple(atom, term);
+  erl_free_term(term);
+}
+
+void send_response_tuple(char* atom_str, ETERM *term) {
+  ETERM *to_send, *tup_list[2];
+  tup_list[0] = erl_mk_atom(atom_str);
+  tup_list[1] = term;
   to_send = erl_mk_tuple(tup_list, 2);
 
   send_response(to_send);
 
   erl_free_term(tup_list[0]);
-  erl_free_term(tup_list[1]);
   erl_free_compound(to_send);
 }
-
 void send_error(char *err_msg) {
   ETERM *tup_list[2];
   ETERM *to_send;
@@ -43,8 +50,7 @@ void send_ok(void) {
 }
 
 void send_response(ETERM *response) {
-  byte buf[2048];
-  bzero(buf, 2048);
-  erl_encode(response, buf);
-  write_cmd(buf, erl_term_len(response));
+  memset(__write_buf, 0, WRITE_BUF_LEN);
+  erl_encode(response, __write_buf);
+  write_cmd(__write_buf, erl_term_len(response));
 }
