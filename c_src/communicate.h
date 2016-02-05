d8feabd59805797da67a241b7f770eb61bbc0197
orotemo <omri.rotem@gmail.com>
2016-02-05 18:39:39 +0200

    Update communicate.h

--- c_src/communicate.h
+++ c_src/communicate.h
@@ -2,8 +2,9 @@
 #define _COMMUNICATE_H
 
 void send_str_tuple(char* atom, char *str);
+void send_response_tuple(char* atom_str, ETERM *term);
 void send_error(char *err_msg);
 void send_ok(void);
 void send_response(ETERM *response);
 
-#endif // _COMMUNICATE_H
+#endif //_COMMUNICATE_H
